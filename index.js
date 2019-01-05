c6fcb02decd5c75201da5da773e7fc973951eceb
Peter Kreppein <pmkreppein@gmail.com>
2019-01-05 01:10:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const driversWithRevenueOver = (drivers, revenue) => {
-  return drivers.filter(function (driver) {return driver.revenue > revenue});
+  return drivers.filter(function (driver) {return driver.revenue > revenue})
 }
-
 const driverNamesWithRevenueOver = (drivers, revenue) => {
   return driversWithRevenueOver(drivers, revenue).map(function (driver) {
-    return driver.name;
-  });
+    return driver.name
+  })
 }
-
 const exactMatch = (drivers, attribute) => {
   return drivers.filter(function (driver) {
-    return driver[Object.keys(attribute)[0]] === attribute[Object.keys(attribute)[0]];
-  });
+    return driver[Object.keys(attribute)[0]] === attribute[Object.keys(attribute)[0]]
+  })
 }
-
 const exactMatchToList = (drivers, attribute) => {
   return exactMatch(drivers, attribute).map(function (driver) {return driver['name']});
 }
